d1673f288be6553c196e2d03560d487a956d3258
Prakhar pal <95201078+Prakharpal94@users.noreply.github.com>
2023-10-30 00:24:55 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 'use strict';
-// # for Selector ID
-// . for Selector CLASSES
-
 //Selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
